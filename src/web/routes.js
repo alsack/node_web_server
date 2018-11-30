2acccbcf7a932a685a90028ffbabf226ae40cf24
Andrew Sack <36677181+alsack@users.noreply.github.com>
2018-11-30 09:52:38 -0500

    Adding routes.js back in
    I'm not sure how this gitignore thing works...

---
+++ src/web/routes.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const rootDir = path.resolve(__dirname, '../..'); //the root of the node_web_server project.
+
+/**
+ * This module defines the routes to different projects. It exports an Array of objects
+ * defining the route path, and the router for that application.  Example:
+ * {
+ *      path: '/example',
+ *      router: path.resolve(rootDir, '../ExampleProject/src/web/router.js')
+ * }
+ * In this example, 'path' is the route path for the root express app.
+ * router must be a express.Router() object.
+ */
+
+const routes = [{
+    path: '/coursera-bootstrap-react',
+    router: path.resolve(rootDir, '../coursera-bootstrap-react/src/server/web/router')
+}];
+
+module.exports = routes;
